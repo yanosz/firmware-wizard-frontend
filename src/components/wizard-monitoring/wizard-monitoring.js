9f2c5921abccbe07c475b6122a85eb95bebb2487
yanosz <github@yanosz.net>
2017-12-01 14:06:13 +0100

    Misc fixes

--- src/components/wizard-monitoring/wizard-monitoring.js
+++ src/components/wizard-monitoring/wizard-monitoring.js
@@ -10,10 +10,6 @@ export default module('app.components.wizard-monitoring', [])
       constructor($scope) {
         'ngInject';
 
-        this.newMonitoring = {
-          server: 'fdd3:5d16:b5dd:fc33:70d0:57ff:febb:8f14',
-        };
-
         $scope.$watch('$ctrl.monitoring', this.updateFromInput.bind(this), true);
         $scope.$watch('$ctrl.newMonitoring', this.updateOutput.bind(this), true);
       }
